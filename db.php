9cf06c5cd5908004486fe40a9a4ba88a181a4940
Luis Esteban <luisestebantorres29@gmail.com>
2021-09-18 10:17:14 -0500

    fue creada la coneccion DB

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+//-============================================================================
+//-============================================================================
+// Ya no me dio tiempo de poner seguridad a la coneccion espero no ser hackeado
+//-============================================================================
+//-============================================================================
+
+$host="bbp0d3urfj9yyznwx31k-mysql.services.clever-cloud.com";
+$user= "uv1pnwix7pexd4jo";
+$clave="2Hl4s4SHLMdIJ1e2SfIJ";
+$bd="bbp0d3urfj9yyznwx31k";
+$conectar = mysqli_connect($host,$user,$clave,$bd);
+
+// session_start();
+
+?>
